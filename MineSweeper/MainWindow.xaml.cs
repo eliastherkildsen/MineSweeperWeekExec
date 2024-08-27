2f18bf4cf8a4a88a35b8a6b4986477756f0242c7
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-08-27 16:31:27 +0200

    Add project files.

---
+++ MineSweeper/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _gameBoardSize;
+        private UniformGrid _grid;
+        private List<List<Button>> _buttons = new List<List<Button>>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGameBoard(10);
+        }
+
+
+        public void InitializeGameBoard(int size) {
+
+            Random rng = new Random();
+
+            _gameBoardSize = size;
+            
+            // assigning grid to gameboard ref from xaml.
+            _grid = gameBoard;
+            _grid.Columns = _gameBoardSize; 
+            _grid.Rows = _gameBoardSize;
+
+            for (int i =  0; i < _gameBoardSize; i++)
+            {
+                _buttons.Add(new List<Button>());
+
+                for (int j = 0; j < _gameBoardSize; j++)
+                {
+                    gameBoard.Children.Add(new Tile(rng.Next(0, 10) == 3));
+                }
+
+               
+
+            }
+
+        } 
+
+
+       
+    }
+}
---
+++ MineSweeper/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MineSweeper
+{
+
+    public delegate void ButtonClicked(int i);
+
+    class Tile : Button
+    {
+
+        public static event ButtonClicked buttonClicked; 
+                    
+        private bool isBomb {get;}
+        private int boombsAround {  get; set; }
+
+        public Tile(bool isBomb) {
+            this.isBomb = isBomb;
+
+            Content = "?";
+
+            if (isBomb)
+            {
+                Content += "&"; 
+            }
+
+        }
+
+
+
+    }
+}
